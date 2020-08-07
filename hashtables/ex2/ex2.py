b35a08fa8bdab8b7698713b5149d91da3dcaeb5f
Ben <bbjberg91@gmail.com>
2020-08-07 13:51:00 -0400

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,17 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    #define route lenght and location
+    route = [None] * length
+    location = {}
+    #loop through our tickets setting ticket source to dest and next location to none
+    for ticket in tickets:
+        location[ticket.source] = ticket.destination
+    next = location["NONE"]
+    #loop over length setting the route index to the next location and the next to the next location in the list 
+    for i in range(0, length):
+        route[i] = next
+        next = location[next]
 
     return route
+
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,24 @@
+
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    cache = {}
+    
+    #loop through arrays
+    for array in arrays:
+        #loop through array values
+        for value in array:
+            #if they're in cache and not results add them to results
+            if value in cache and value not in result:
+                result.append(value)
+            else:
+                cache[value] = True
 
     return result
+
 
 
 if __name__ == "__main__":
